08860abd8e3fc850a20b10b3dc21903b5f0a179b
Sebastian <salvarezocampo@hotmail.com>
2022-10-10 07:30:41 -0500

    adding bottom nav bar

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -9,6 +9,32 @@ class MainScreen extends StatelessWidget {
       body: Center(
         child: Text('Main Screen'),
       ),
+      bottomNavigationBar: _Navigation(),
+    );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  const _Navigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          label: 'For you',
+          icon: Icon(Icons.account_circle_outlined),
+          activeIcon: Icon(Icons.account_circle),
+        ),
+        BottomNavigationBarItem(
+          label: 'Headlines',
+          icon: Icon(Icons.feed_outlined),
+          activeIcon: Icon(Icons.feed),
+        ),
+      ],
     );
   }
 }
