b46075a81172b192933c25e495a22c38c925f845
Sebastian <salvarezocampo@hotmail.com>
2022-10-14 21:36:26 -0500

    fixing news_list

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -15,8 +15,6 @@ class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final newsService = Provider.of<NewsService>(context);
-
     return ListView.builder(
       physics: const BouncingScrollPhysics(),
       itemCount: news.length,
@@ -24,46 +22,16 @@ class NewsList extends StatelessWidget {
 
         switch (type) {
           case 'allTiny':
-            return GestureDetector(
-              onTap: () {
-                newsService.selectedNew = news[index];
-                Navigator.pushNamed(context, 'new');
-              },
-              child: _SecondaryNew( newArticle: news[index],)
-            );
+            return _SecondaryNew( newArticle: news[index],);
           case 'allBig':
-            return GestureDetector(
-                onTap: () {
-                  newsService.selectedNew = news[index];
-                  Navigator.pushNamed(context, 'new');
-                },
-                child: _TopBarCard( newArticle: news[index], index: index)
-              );
+            return _TopBarCard( newArticle: news[index], index: index);
           case 'firstBig':
             if(index == 0){
-              return GestureDetector(
-                onTap: () {
-                  newsService.selectedNew = news[index];
-                  Navigator.pushNamed(context, 'new');
-                },
-                child: _TopBarCard( newArticle: news[index], index: index)
-              );
+              return _TopBarCard( newArticle: news[index], index: index);
             }
-            return GestureDetector(
-              onTap: () {
-                newsService.selectedNew = news[index];
-                Navigator.pushNamed(context, 'new');
-              },
-              child: _SecondaryNew( newArticle: news[index],)
-            );
+            return _SecondaryNew( newArticle: news[index],);
           default:
-            return GestureDetector(
-              onTap: () {
-                newsService.selectedNew = news[index];
-                Navigator.pushNamed(context, 'new');
-              },
-              child: _SecondaryNew( newArticle: news[index],)
-            );
+            return _SecondaryNew( newArticle: news[index],);
         }
 
       },
@@ -82,60 +50,69 @@ class _SecondaryNew extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 12
-      ),
-      child: Row(
-      
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            child: SizedBox(
-              height: 60,
-              width: 90,
-              /*child: Image(
-                image: newArticle.urlToImage != null ? NetworkImage(newArticle.urlToImage!) : NetworkImage('https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'),
-                fit: BoxFit.cover,
-              ),*/
-              child: ImageLoader(
-                urlImage: newArticle.urlToImage, 
-                heightContainer: 60, 
-                sizeLoader: 15,
-                widthContainer: 90,
+
+    final newsService = Provider.of<NewsService>(context);
+
+    return GestureDetector(
+      onTap: () {
+        newsService.selectedNew = newArticle;
+        Navigator.pushNamed(context, 'new');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 12
+        ),
+        child: Row(
+        
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              child: SizedBox(
+                height: 60,
+                width: 90,
+                /*child: Image(
+                  image: newArticle.urlToImage != null ? NetworkImage(newArticle.urlToImage!) : NetworkImage('https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'),
+                  fit: BoxFit.cover,
+                ),*/
+                child: ImageLoader(
+                  urlImage: newArticle.urlToImage, 
+                  heightContainer: 60, 
+                  sizeLoader: 15,
+                  widthContainer: 90,
+                ),
               ),
             ),
-          ),
-      
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    newArticle.title,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      fontSize: 15,
+        
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      newArticle.title,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        fontSize: 15,
+                      ),
                     ),
-                  ),
-      
-                  Text(
-                    newArticle.source.name,
-                    maxLines: 2,
-                    style: const TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      color: Colors.white54
+        
+                    Text(
+                      newArticle.source.name,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        color: Colors.white54
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -154,83 +131,92 @@ class _TopBarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 12
-      ),
-      child: Stack(
-        alignment: AlignmentDirectional.bottomStart,
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            child: SizedBox(
-              height: 250,
-              width: double.infinity,
-              // decoration: BoxDecoration(
-              //   image: DecorationImage(
-              //     image: newArticle.urlToImage != null ? NetworkImage(newArticle.urlToImage!) : NetworkImage('https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'),
-              //     fit: BoxFit.cover,
-              //   ),
-              // ),
-              child: ImageLoader(
-                urlImage: newArticle.urlToImage, 
-                heightContainer: 250, 
-                sizeLoader: 50,
-              ),
-            ),
-          ),
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-            child: Container(
-              width: double.infinity,
-              height: 250,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.black.withOpacity(0.5),
-                    Colors.transparent,
-                  ],
-                )
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _InfoTag(
-                      info: newArticle.source.name, 
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
 
-                    _InfoTag(
-                      info: DateFormat.yMMMd().format(newArticle.publishedAt), 
-                      color: Colors.black.withOpacity(0.6)
-                    )
-                  ],
+    final newsService = Provider.of<NewsService>(context);
+
+    return GestureDetector(
+      onTap: () {
+        newsService.selectedNew = newArticle;
+        Navigator.pushNamed(context, 'new');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 12
+        ),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              child: SizedBox(
+                height: 250,
+                width: double.infinity,
+                // decoration: BoxDecoration(
+                //   image: DecorationImage(
+                //     image: newArticle.urlToImage != null ? NetworkImage(newArticle.urlToImage!) : NetworkImage('https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'),
+                //     fit: BoxFit.cover,
+                //   ),
+                // ),
+                child: ImageLoader(
+                  urlImage: newArticle.urlToImage, 
+                  heightContainer: 250, 
+                  sizeLoader: 50,
                 ),
-                Text(
-                  newArticle.title,
-                  maxLines: 2,
-                  style: const TextStyle(
-                    overflow: TextOverflow.ellipsis,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-          
-        ],
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              child: Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black.withOpacity(0.5),
+                      Colors.transparent,
+                    ],
+                  )
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _InfoTag(
+                        info: newArticle.source.name, 
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+    
+                      _InfoTag(
+                        info: DateFormat.yMMMd().format(newArticle.publishedAt), 
+                        color: Colors.black.withOpacity(0.6)
+                      )
+                    ],
+                  ),
+                  Text(
+                    newArticle.title,
+                    maxLines: 2,
+                    style: const TextStyle(
+                      overflow: TextOverflow.ellipsis,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
       ),
     );
   }
